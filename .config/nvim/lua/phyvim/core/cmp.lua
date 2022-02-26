4870c86254f2c945d76e81408f02424ec1e7b075
phytech-r0 <phystecharda@gmail.com>
2022-02-26 01:06:42 +0000

    cmp update

--- .config/nvim/lua/phyvim/core/cmp.lua
+++ .config/nvim/lua/phyvim/core/cmp.lua
@@ -63,7 +63,7 @@ cmp.setup {
     },
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    --["<CR>"] = cmp.mapping.confirm { select = false },
+    ["<CR>"] = cmp.mapping.confirm { select = false },
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.confirm({ select = true })
