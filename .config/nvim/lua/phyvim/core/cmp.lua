89b7d8b0b9d7e84898937cd50e5dbc72527bd643
phytech-r0 <phystecharda@gmail.com>
2022-03-01 23:04:45 +0000

    nvim upd

--- .config/nvim/lua/phyvim/core/cmp.lua
+++ .config/nvim/lua/phyvim/core/cmp.lua
@@ -50,6 +50,7 @@ cmp.setup {
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
+	preselect = cmp.PreselectMode.None,
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
 		["<C-j>"] = cmp.mapping.select_next_item(),
