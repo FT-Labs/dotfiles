b54ce025c19c0bc9b242788950648d1dfbb7cd45
arda <phystecharda@gmail.com>
2022-03-09 00:30:14 +0300

    nvim upd

--- .config/nvim/lua/phyvim/core/project.lua
+++ .config/nvim/lua/phyvim/core/project.lua
@@ -5,7 +5,7 @@ local opts = {
 	---@usage set to true to disable setting the current-woriking directory
 	--- Manual mode doesn't automatically change your root directory, so you have
 	--- the option to manually do so using `:ProjectRoot` command.
-	manual_mode = false,
+	manual_mode = true,
 
 	---@usage Methods of detecting the root directory
 	--- Allowed values: **"lsp"** uses the native neovim lsp
--- .config/nvim/lua/phyvim/core/treesitter.lua
+++ .config/nvim/lua/phyvim/core/treesitter.lua
@@ -65,7 +65,7 @@ local opts = {
     rainbow = {
       enable = true,
       extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-      max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+      max_file_lines = 5000, -- Do not enable for files with more than 1000 lines, int
     },
   }
 
