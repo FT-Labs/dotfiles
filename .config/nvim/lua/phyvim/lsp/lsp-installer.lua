e6a1b01ad09a947878b53ae73ef8aa0400a8ceb1
arda <phystecharda@gmail.com>
2022-03-15 01:23:53 +0300

    dots upd

--- .config/nvim/lua/phyvim/lsp/lsp-installer.lua
+++ .config/nvim/lua/phyvim/lsp/lsp-installer.lua
@@ -26,6 +26,12 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	 end
 
+
+	 if server.name == "emmet-ls" then
+	 	local emmet_opts = require("phyvim.lsp.settings.emmet-ls")
+	 	opts = vim.tbl_deep_extend("force", emmet_opts, opts)
+	 end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
