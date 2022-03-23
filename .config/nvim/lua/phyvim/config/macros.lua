0bea678252ef46c75f74e0eff24afb6302faad37
arda <phystecharda@gmail.com>
2022-03-23 15:46:49 +0300

    macros change to autocmd

--- .config/nvim/lua/phyvim/config/macros.lua
+++ .config/nvim/lua/phyvim/config/macros.lua
@@ -12,13 +12,13 @@ vim.cmd([[ let @1=":call CppComp() \<CR>G:66\<CR>" ]])
 -- All previous macros have been changed to autocmd, @g macro will run current file
 vim.cmd [[
 	augroup run_file
-		autocmd FileType java let @g=":w\<CR>:vsp | terminal java %\<CR>i"
-		autocmd FileType python let @g=":w\<CR>:vsp |terminal python %\<CR>i" 
-		autocmd FileType asm let @g=":w\<CR> :!nasm -f elf64 -o out.o % && ld out.o -o a.out \<CR> | :vsp |terminal ./a.out\<CR>i" 
-		autocmd FileType cpp let @g=":w\<CR> :!g++ %\<CR> | :vsp |terminal ./a.out\<CR>i" 
-		autocmd FileType c let @g=":w\<CR> :!gcc %\<CR> | :vsp |terminal ./a.out\<CR>i" 
-		autocmd FileType go let @g=":w\<CR> :vsp | terminal go run % \<CR>i" 
-		autocmd FileType javascript let @g=":w\<CR> :vsp | terminal node % \<CR>i" 
-		autocmd FileType html let @g=":w\<CR> :silent !chromium % \<CR>"
+		autocmd BufEnter *.java let @g=":w\<CR>:vsp | terminal java %\<CR>i"
+		autocmd BufEnter *.py let @g=":w\<CR>:vsp |terminal python %\<CR>i" 
+		autocmd BufEnter *.asm let @g=":w\<CR> :!nasm -f elf64 -o out.o % && ld out.o -o a.out \<CR> | :vsp |terminal ./a.out\<CR>i" 
+		autocmd BufEnter *.cpp let @g=":w\<CR> :!g++ %\<CR> | :vsp |terminal ./a.out\<CR>i" 
+		autocmd BufEnter *.c let @g=":w\<CR> :!gcc %\<CR> | :vsp |terminal ./a.out\<CR>i" 
+		autocmd BufEnter *.go let @g=":w\<CR> :vsp | terminal go run % \<CR>i" 
+		autocmd BufEnter *.js let @g=":w\<CR> :vsp | terminal node % \<CR>i" 
+		autocmd BufEnter *.html let @g=":w\<CR> :silent !chromium % \<CR>"
 	augroup end
 ]]
