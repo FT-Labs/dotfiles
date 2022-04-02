45bf8ac722f2121a145a3f8cab1310043dbb4ec4
arda <phystecharda@gmail.com>
2022-04-03 00:32:16 +0300

    theme changed to onedarker

--- .config/nvim/lua/phyvim/config/init.lua
+++ .config/nvim/lua/phyvim/config/init.lua
@@ -1,18 +1,21 @@
 local M = {}
 
-function M:load()
-  require "phyvim.config.keymaps"
-  require "phyvim.config.options".load_options()
-	require "phyvim.config.autocmds"
-
-  vim.g.tokyonight_style = "night"
-  local colorscheme = "dracula"
+function load_colorscheme()
+  -- vim.g.tokyonight_style = "night"
+  local colorscheme = "onedarker"
 
 	local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 	if not status_ok then
 		vim.notify("colorscheme " .. colorscheme " not found!")
 		return
 	end
+end
+
+function M:load()
+  require "phyvim.config.keymaps"
+  require "phyvim.config.options".load_options()
+	require "phyvim.config.autocmds"
+	load_colorscheme()
 end
 
 return M
