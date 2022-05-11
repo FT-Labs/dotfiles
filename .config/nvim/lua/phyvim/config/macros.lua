1f97891d8bec38ed472cbd632a07bb26b5178d85
arda <phystecharda@gmail.com>
2022-05-11 10:44:12 +0300

    ls aliased to exa

--- .config/nvim/lua/phyvim/config/macros.lua
+++ .config/nvim/lua/phyvim/config/macros.lua
@@ -13,7 +13,7 @@ vim.cmd [[
 		autocmd BufEnter *.py let @g=":w\<CR>:vsp |terminal python %\<CR>i"
 		autocmd BufEnter *.asm let @g=":w\<CR> :!nasm -f elf64 -o out.o % && ld out.o -o a.out \<CR> | :vsp |terminal ./a.out\<CR>i"
 		autocmd BufEnter *.cpp let @g=":w\<CR> :!g++ -std=c++17 -O3 %\<CR> | :vsp |terminal ./a.out\<CR>i"
-		autocmd BufEnter *.c let @g=":w\<CR> :!gcc -O3 -std=c99 -pedantic -Wall -Wextra %\<CR> | :vsp |terminal ./a.out\<CR>i"
+		autocmd BufEnter *.c let @g=":w\<CR> :!gcc -O3 -std=c99 -Wno-deprecated-declarations -pedantic -Wall -Wextra %\<CR> | :vsp |terminal ./a.out\<CR>i"
 		autocmd BufEnter *.go let @g=":w\<CR> :vsp | terminal go run % \<CR>i"
 		autocmd BufEnter *.js let @g=":w\<CR> :vsp | terminal node % \<CR>i"
 		autocmd BufEnter *.html let @g=":w\<CR> :silent !chromium % \<CR>"
