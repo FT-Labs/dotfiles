1ef36af96222c28904bf6bbcc0073a89e1da95db
arda <phystecharda@gmail.com>
2022-05-18 01:39:00 +0300

    small fix on trailing lines

--- .config/nvim/lua/phyvim/config/autocmds.lua
+++ .config/nvim/lua/phyvim/config/autocmds.lua
@@ -33,6 +33,5 @@ vim.cmd [[
  	autocmd BufWritePre * let currPos = getpos(".")
 	autocmd BufWritePre * %s/\s\+$//e
 	autocmd BufWritePre * %s/\n\+\%$//e
-	autocmd BufWritePre *.[ch] %s/\%$/\r/e
  	autocmd BufWritePre * cal cursor(currPos[1], currPos[2])
 ]]
