625831503d7edb47bfe5eb184367c39dc30ebb31
phytech-r0 <phystecharda@gmail.com>
2022-06-13 02:53:28 +0000

    scheme

--- .config/nvim/lua/phyvim/config/init.lua
+++ .config/nvim/lua/phyvim/config/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 local function load_colorscheme()
-  vim.g.tokyonight_style = "night"
-  local colorscheme = "moonlight"
+--  vim.g.tokyonight_style = "night"
+  local colorscheme = "tokyonight"
 
   local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
   if not status_ok then
