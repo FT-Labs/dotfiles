3c770816f128fd86ab1d9e481f693fa8b0a47b12
Arda Atci <phystecharda@gmail.com>
2022-09-24 20:33:35 +0300

    primitive macro

--- .config/nvim/lua/phyvim/config/init.lua
+++ .config/nvim/lua/phyvim/config/init.lua
@@ -1,8 +1,7 @@
 local M = {}
 
 local function load_colorscheme()
---  vim.g.tokyonight_style = "night"
-  local colorscheme = "tokyonight"
+  local colorscheme = "catppuccin"
 
   local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
   if not status_ok then
